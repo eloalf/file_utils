0a4c65456c23c73d76de951acef31348618c8162
Alfred Schmiedl <84848121+eloalf@users.noreply.github.com>
2021-11-18 13:15:57 +0100

    Add files via upload

--- memalloc2.c
+++ memalloc2.c
@@ -1,49 +1,64 @@
 
-/* memalloc2.c                                                                        */
-/* Using malloc() to claim/release large dynamic memory in chunks                     */
-/* A. Schmiedl , FAU / Biochemistry Division , 2021-11-16 / 2021-11-17                */
-/* Used in conjunction with bash script memalloc2_reg.sh                              */
+/* memalloc2.c                                                                                          */
+/* A. Schmiedl , FAU / Biochemistry Division , 2021-11-17 / 2021-11-18                                  */
+/* Using calloc() to claim/release large dynamic memory in big chunks                                   */
+/* malloc() would reserve but not use up memory unless you actually put data into it (lazy allocation), */
+/* even calloc() doesn't garantee to use up the RAM, so actually fill it with random data. Read:        */
+/* https://stackoverflow.com/questions/19991623/why-is-malloc-not-using-up-the-memory-on-my-computer    */
+/* https://www.thegeekdiary.com/understanding-proc-meminfo-file-analyzing-memory-utilization-in-linux/  */
+/* Use in conjunction with bash script memalloc2_reg.sh                                                 */
 
-#define  BNUM  100000000       /* number of bytes to allocate per memory block: 100M  */
+#define  BNUM  100000000                         /* number of bytes to allocate per memory block: 100M  */
 #include <stdio.h>
-#include <stdlib.h>                    /* for: malloc(), calloc(), realloc(), free()  */
-#include <unistd.h>                                                  /* for: sleep()  */
+#include <stdlib.h>                                      /* for: malloc(), calloc(), realloc(), free()  */
+#include <unistd.h>                                                                    /* for: sleep()  */
+#include <time.h>                                                             /* for srand() and rand() */
 
 int main () {
 
-    int i, n;
-    char *parray[20];                                /* array of 20 pointers to char  */
+    int n;                                                                 /* n = current block number  */
+    unsigned long i;                                                           /* i = index into block  */
+    char *parray[20];                                                  /* array of 20 pointers to char  */
 
-    system("touch memalloc2_started");            /* mark start of program with file  */
-    for (n=0; n<20; n++)  parray[i]=NULL;             /* initialize pointers to NULL  */
+    system("touch memalloc2_started");                     /* mark start of program by creating a file  */
+    for (n=0; n<20; n++)  parray[n]=NULL;                           /* initialize all pointers to NULL  */
 
-                         /* Memory allocation on the heap in a time controlled loop:  */
-    printf("--> Claim 20*%d bytes of memory block by block in 4s time increments: \n", BNUM);
-    printf("    Monitoring method: \n");
-    printf("    Get PID of process with:  ps -ea | grep memalloc2  \n");
-    printf("    Print memory usage with:  sudo pmap <pid> | tail -n 1 | awk '/total/{print $2}'  \n");
+                                           /* Memory allocation on the heap in a time controlled loop:  */
+    printf("--> Allocate 20*%d bytes of memory block by block in 4s time increments: \n", BNUM);
+    printf("    Monitoring examples: \n");
+    printf("      Get PID of process with:  ps -ea | grep memalloc2  \n");
+    printf("      Print memory usage with:  pmap <pid> | tail -n 1 | awk '/total/{print $2}'  \n");
+    printf("      Print available memory:   awk '/MemAvailable/ {print $2,$3}' /proc/meminfo  \n");
 
-    for (n=0; n<20; n++)  {
-        sleep(4);                       /* wait 10s before each new block allocation  */
-        parray[n] = (char *) malloc(BNUM);           /* claiming BNUM bytes of space  */
+    srand(time(0));                                                /* Seed the random number generator  */
+
+    for (n=0; n<20; n++)  {                                             /* Allocation and filling loop  */
+        sleep(4);                                          /* wait 4s before each new block allocation  */
+        parray[n] = (char *) calloc(BNUM, sizeof(char));                    /* Get BNUM bytes of space  */
         if (parray[n] == NULL) {
             fprintf(stderr, "--> Error! Memory allocation for block %d failed. \n", n);
             return(-(n+1));
-        } else
-            printf("    block %d allocated. \n", n+1);
+        }
+        else  {
+            printf("    block %2d allocated, starting at location %p , ", n+1, (void *)parray[n] );
+            printf(" fill it with random bytes ... \n" );
+            for (i=0; i<BNUM; i++)  {                                           /* Fill each block ...  */
+                * (parray[n] + i) = (char)(rand() % 256);                     /* ... with random bytes  */
+            }
+        }
     }
 
     printf("--> Now release the allocated memory blocks in 2s time steps: \n");
     for (n=19; n>=0; n--)  {
-        sleep(2);                     /* wait 2s before giving each block back to OS  */
+        sleep(2);                                       /* wait 2s before giving each block back to OS  */
         free(parray[n]);
         printf("    block %d freed. \n", n+1);
     }
 
-    printf("--> All allocated memory blocks released - termination in 30s. \n");
-    sleep(28);           /* wait 28s with all memory released before removing marker  */
-    system("rm memalloc2_started");                            /* remove marker file  */
-    sleep(2);                                          /* wait 2s before termination  */
+    printf("--> All allocated memory blocks released - termination in 20s. \n");
+    sleep(18);                             /* wait 18s with all memory released before removing marker  */
+    system("rm memalloc2_started");                                              /* remove marker file  */
+    sleep(2);                                                            /* wait 2s before termination  */
 
     return(0);
 }
--- memalloc2_reg.sh
+++ memalloc2_reg.sh
@@ -1,10 +1,9 @@
 
 #!/bin/bash
 # memalloc2_reg.sh
-# Shell script to record current total memory usage of process 'memalloc2'
+# Store memory usage of program 'memalloc2' as well as available memory in 1s increments into log file 'memalloc2_plt.dat'
 # Used in conjunction with program 'memalloc2' (compiled 'memalloc2.c')
-# Store memory usage of program 'memalloc2' in 1s increments into log file 'memalloc2_plt.dat'
-# A. Schmiedl, FAU / Biochemistry Division, 2021-11-16 / 2021-11-17
+# A. Schmiedl, FAU / Biochemistry Division, 2021-11-17 / 2021-11-18
 
 # Wait for file 'memalloc2_started' marking the start of program 'memalloc2.c' :
 echo "Waiting for 'memalloc2.c' to start ..."
@@ -15,6 +14,7 @@ done
 
 counter=1
 PROCNUM=$(ps -ea | awk '/memalloc2/{print $1}')
+echo "PID = $PROCNUM found! "
 
 # Used in an earlier version to terminate script when process was not yet started (checking for a valid PID number):
 #if ! [[ "$PROCNUM" =~ ^[0-9]+$ ]]
@@ -36,7 +36,7 @@ do
     CURRT=$(date +%s)
     if [ $CURRT -gt $LASTT ]
     then    # execute this block once every second:
-        echo "$((CURRT-STARTT)) $(pmap $PROCNUM | tail -n 1 | awk '/total/{print $2}') $(free -b | awk '/Mem/ {print $4}')" >> memalloc2_mem.dat
+        echo "$((CURRT-STARTT)) $(pmap $PROCNUM | tail -n 1 | awk '/total/{print $2}') $(awk '/MemAvailable/ {print $2}' /proc/meminfo )" >> memalloc2_mem.dat
         LASTT=$CURRT
             # counter is incrementing in 1s intervals with some little jitter but long term accuracy
         ((counter++))
@@ -47,8 +47,8 @@ done
 # remove 'K' and display valid lines only into new file memalloc2_plt.dat :
 perl -pe 's/K//g' memalloc2_mem.dat | awk 'NF==3{print}' > memalloc2_plt.dat
 rm memalloc2_mem.dat
-echo "End time of recording  = $(date)"
+echo "End time of recording   = $(date)"
 echo $(wc memalloc2_plt.dat | awk '{print $1, "entries have been recorded into file memalloc2_plt.dat "}')
-echo "File  memalloc2_plt.dat  contains seconds + KB values + free usable RAM, may be plotted with command: "
-echo "gnuplot --persist -e \"set grid ; set title 'process memory KB' ; plot 'memalloc2_plt.dat' using 1:2 with lines\" "
-echo "gnuplot --persist -e \"set grid ; set title 'free memory B' ;     plot 'memalloc2_plt.dat' using 1:3 with lines\" "
+echo "File memalloc2_plt.dat contains seconds + KB values + free usable RAM, may be plotted with command: "
+echo "gnuplot --persist -e \"set grid ; set title 'process memory KB' ;   plot 'memalloc2_plt.dat' using 1:2 with lines\" "
+echo "gnuplot --persist -e \"set grid ; set title 'available memory KB' ; plot 'memalloc2_plt.dat' using 1:3 with lines\" "
