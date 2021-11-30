0e0f04e9aa51d0883fe9a1c2b69a95893f052014
Alfred Schmiedl <84848121+eloalf@users.noreply.github.com>
2021-11-30 14:48:01 +0100

    Add files via upload

--- memalloc2.c
+++ memalloc2.c
@@ -1,9 +1,11 @@
 
 /* memalloc2.c                                                                                          */
-/* A. Schmiedl , FAU / Biochemistry Division , 2021-11-18                                               */
-/* Using calloc() to claim/release large dynamic memory in big chunks                                   */
+/* A. Schmiedl , FAU / Biochemistry Division , 2021-11-18 / 2021-11-30                                  */
+/* Using calloc() to claim/release large dynamic memory in big chunks.                                  */
 /* malloc() would reserve but not use up memory unless you actually put data into it (lazy allocation), */
 /* even calloc() doesn't garantee to use up the RAM, so actually fill it with random data.              */
+/* For every next block, store inverted bytes from the previous one, and compare before releasing it,   */
+/* which is a kind of poor man's memory tester.                                                         */
 /* Use in conjunction with bash script memalloc2_reg.sh                                                 */
 
 #define  BNUM  100000000                         /* number of bytes to allocate per memory block: 100M  */
@@ -16,6 +18,7 @@ int main () {
 
     int n;                                                                 /* n = current block number  */
     unsigned long i;                                                           /* i = index into block  */
+    unsigned long identerr=0;                                    /* counter for memory cell comparision */
     char *parray[20];                                                  /* array of 20 pointers to char  */
 
     system("touch memalloc2_started");                     /* mark start of program by creating a file  */
@@ -27,33 +30,51 @@ int main () {
     printf("      Get PID of process with:  ps -ea | grep memalloc2  \n");
     printf("      Print memory usage with:  pmap <pid> | tail -n 1 | awk '/total/{print $2}'  \n");
     printf("      Print available memory:   awk '/MemAvailable/ {print $2,$3}' /proc/meminfo  \n");
+    printf("    Automated monitoring with script 'memalloc2_reg.sh': start before 'memalloc2'  \n");
 
     srand(time(0));                                                /* Seed the random number generator  */
 
     for (n=0; n<20; n++)  {                                             /* Allocation and filling loop  */
-        sleep(4);                                          /* wait 4s before each new block allocation  */
-        parray[n] = (char *) calloc(BNUM, sizeof(char));                    /* Get BNUM bytes of space  */
+        sleep(4);                                              /* wait 4s before next block allocation  */
+        parray[n] = (char *) calloc(BNUM, sizeof(char));                 /* Demand BNUM bytes of space  */
         if (parray[n] == NULL) {
             fprintf(stderr, "--> Error! Memory allocation for block %d failed. \n", n);
             return(-(n+1));
         }
         else  {
-            printf("    block %2d allocated, starting at location %p , ", n+1, (void *)parray[n] );
-            printf(" fill it with random bytes ... \n" );
-            for (i=0; i<BNUM; i++)  {                                           /* Fill each block ...  */
-                * (parray[n] + i) = (char)(rand() % 256);                     /* ... with random bytes  */
+            if((n & 1) == 0)  {                                                    /* Even block number */
+                printf("    even block %2d allocated, starting at location %p , ", n, (void *)parray[n] );
+                printf(" fill it with random bytes ... \n" );
+                for (i=0; i<BNUM; i++)
+                    * (parray[n] + i) = (char)(rand() % 256);     /* Fill even block with random bytes  */
+            }
+            else  {                                                                 /* Odd block number */
+                printf("    odd  block %2d allocated, starting at location %p , ", n, (void *)parray[n] );
+                printf(" fill it with inverted bytes from previous block ... \n" );
+                for (i=0; i<BNUM; i++)  {
+                    *(parray[n] + i) = *(parray[n-1] + i) ^ 0xFF;  /* Fill odd block w. inverted bytes  */
+                    if ((n==15) && ((i==55444333) || (i==12345678)))
+                        *(parray[n] + i) = 0x5C;         /* In 1 block deliberately introduce 2 errors  */
+                }
             }
         }
     }
 
-    printf("--> Now release the allocated memory blocks in 2s time steps: \n");
+    printf("--> Now check contents and release the allocated blocks in 2s time steps: \n");
     for (n=19; n>=0; n--)  {
         sleep(2);                                       /* wait 2s before giving each block back to OS  */
+        if((n & 1) == 1)  {      /* check content of 2 adjacent blocks which should match when inverted */
+            for (i=0; i<BNUM; i++)  {
+                if ( ( (char) *(parray[n]+i)) != ( (char) (*(parray[n-1]+i)^0xFF)) )
+                    identerr++;
+            }            
+        }
         free(parray[n]);
-        printf("    block %d freed. \n", n+1);
+        printf("    block %d released. \n", n);
     }
 
-    printf("--> All allocated memory blocks released - termination in 20s. \n");
+    printf("--> All allocated memory blocks freed - termination in 20s. \n");
+    printf("    Content error counts (2 errors expected) = %lu . \n", identerr);
     sleep(18);                             /* wait 18s with all memory released before removing marker  */
     system("rm memalloc2_started");                                              /* remove marker file  */
     sleep(2);                                                            /* wait 2s before termination  */
